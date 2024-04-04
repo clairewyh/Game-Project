08de61dfac39dd0f2ec7b5182e3c5a47f1d58b1e
ccelinaw <164883843+ccelinaw@users.noreply.github.com>
2024-04-04 10:18:37 -0400

    Create startinghomepage.py

---
+++ startinghomepage.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.button import Button
+
+Builder.load_file('main.kv')
+
+class Homepage(BoxLayout):
+    pass
+
+class RoundedButton(Button):
+    pass
+
+class FitnessApp(App):
+    def build(self):
+        return Homepage()
+
+if __name__ == '__main__':
+    application = FitnessApp()
+    Window.size=(397,697)
+    Window.clearcolor = 0.2, 0.8, 1, 1
+    application.run()
