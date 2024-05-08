a1417ccf9eb800688be3b6720e61310103812705
ccelinaw <164883843+ccelinaw@users.noreply.github.com>
2024-05-08 16:49:53 -0400

    Update main.py

--- Master Copy/main.py
+++ Master Copy/main.py
@@ -1,4 +1,6 @@
 import json
+from random import randint
+import csv
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
@@ -6,6 +8,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
 
 Builder.load_file('startinghomepage.kv')
 Builder.load_file('loginpage.kv')
@@ -15,6 +18,9 @@ class Homepage(BoxLayout):
     pass
 
 class UserButton(Button):
+    pass
+
+class ClassButton(Button):
     pass
 
 class LogPage(BoxLayout):
@@ -74,6 +80,101 @@ class LogPage(BoxLayout):
 class ClassPage(BoxLayout):
     pass
 
+class TeacherClassPage(ClassPage):
+    class_data = {}  
+
+    def open_popup(self):
+        popup_layout = BoxLayout(orientation="vertical", padding="10dp")
+        self.class_name_input = TextInput(hint_text="Enter class name", multiline=False, size_hint_y=None, height="40dp")
+        # Listen for key press event in TextInput
+        self.class_name_input.bind(on_text_validate=self.add_class)
+        popup_layout.add_widget(self.class_name_input)
+        # Set background color for the "Add Class" button
+        add_class_button = Button(text="Add Class", on_press=self.add_class, background_color=(0.2, 0.6, 1, 1))
+        popup_layout.add_widget(add_class_button)
+        popup = Popup(title="New Class", content=popup_layout, size_hint=(None, None), size=("300dp", "200dp"))
+        popup.open()
+ 
+    def add_class(self, instance):
+        
+        class_name = self.class_name_input.text
+        if class_name:
+            code = self.generate_unique_code()
+            class_button = ClassButton(text=f"{class_name}: {code}", bold=True)
+            self.ids.classes_layout.add_widget(class_button)
+            self.class_data[class_name] = code
+        
+        #write to csv file
+        with open('class_data.csv', 'a') as file:  # 'a' to append to the file
+            file.write(f"{class_name},{code}\n")
+   
+    def generate_unique_code(self):
+        code = str(randint(10000, 99999))
+        return code
+
+class StudentClassPage(ClassPage):
+    class_data = {}
+
+    def go_back_to_login(self):
+        App.get_running_app().switch_to_login()
+    
+    def open_popup(self):
+        popup_layout = BoxLayout(orientation="vertical", padding="10dp")
+        self.class_code_input = TextInput(hint_text="Enter class code", multiline=False, size_hint_y=None, height="40dp")
+        self.student_name_input = TextInput(hint_text="Enter your name", multiline=False, size_hint_y=None, height="40dp")
+        
+        # Listen for key press event in TextInput
+        self.class_code_input.bind(on_text_validate=self.join_class)
+        popup_layout.add_widget(self.class_code_input)
+        popup_layout.add_widget(self.student_name_input)
+        # Set background color for the "Join Class" button
+        join_class_button = Button(text="Join Class", on_press=self.join_class, background_color=(0.2, 0.6, 1, 1))
+        popup_layout.add_widget(join_class_button)
+        popup = Popup(title="Join Class", content=popup_layout, size_hint=(None, None), size=("300dp", "200dp"))
+        popup.open()
+
+    def join_class(self, instance):
+        class_code = self.class_code_input.text
+        student_name = self.student_name_input.text
+        if class_code and student_name:
+            class_name = self.validate_class_code(class_code)
+            if class_name:
+                class_button = ClassButton(text=f"{class_name}: {class_code}", bold=True)
+                self.ids.classes_layout.add_widget(class_button)
+                self.add_student_to_class_list(class_code, student_name)
+            else:
+                # Show error message if class code is not valid
+                error_label = Label(text="Invalid class code. Please try again.", color=(1, 0, 0, 1))
+                popup_layout = Popup(title="Error", content=error_label, size_hint=(None, None), size=("300dp", "150dp"))
+                popup_layout.open()
+        else:
+            # Show error message if class code or student name is empty
+            error_label = Label(text="Please enter class code and your name.", color=(1, 0, 0, 1))
+            popup_layout = Popup(title="Error", content=error_label, size_hint=(None, None), size=("300dp", "150dp"))
+            popup_layout.open()
+            
+    def add_student_to_class_list(self, class_code, student_name):
+        with open('class_list.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([class_code, student_name])
+      
+    def fetch_class_data(self):        
+        # Clear existing class data
+        self.class_data.clear()
+        # Read class data from CSV file
+        with open('class_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                class_name, class_code = row
+                self.class_data[class_name] = class_code
+        
+    def validate_class_code(self, class_code_input):
+        self.fetch_class_data()
+        if class_code_input in self.class_data.values():
+            for class_name, class_code in self.class_data.items():
+                if class_code == class_code_input:
+                    return class_name 
+
 class FitnessApp(App):
     def build(self):
         self.homepage = Homepage()
@@ -93,18 +194,15 @@ class FitnessApp(App):
         self.homepage.add_widget(self.loginpage)
 
     def switch_to_teacher_classpage(self):
-        self.homepage.clear_widgets()
-        self.homepage.add_widget(TeacherClassPage())
+        self.root.clear_widgets()  # Clear all widgets
+        teacher_classpage = TeacherClassPage()  # Initialize the TeacherClassPage
+        self.root.add_widget(teacher_classpage)
 
     def switch_to_student_classpage(self):
-        self.homepage.clear_widgets()
-        self.homepage.add_widget(StudentClassPage())
+        self.root.clear_widgets()  # Clear all widgets
+        student_classpage = StudentClassPage()
+        self.root.add_widget(student_classpage)
 
-class TeacherClassPage(BoxLayout):
-    pass
-
-class StudentClassPage(BoxLayout):
-    pass
 
 if __name__ == '__main__':
     Window.size=(397,697)
