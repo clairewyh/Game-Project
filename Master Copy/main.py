a2811e651e0104b8dd158e92de373b65d06995c4
ccelinaw <164883843+ccelinaw@users.noreply.github.com>
2024-05-08 15:39:58 -0400

    Update main.py

--- Master Copy/main.py
+++ Master Copy/main.py
@@ -1,15 +1,15 @@
 import json
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.popup import Popup
-from kivy.graphics import Color, Rectangle
-from kivy.core.window import Window
-from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.core.window import Window
 
 Builder.load_file('startinghomepage.kv')
 Builder.load_file('loginpage.kv')
+Builder.load_file('homepagedesign.kv')
 
 class Homepage(BoxLayout):
     pass
@@ -18,6 +18,10 @@ class UserButton(Button):
     pass
 
 class LogPage(BoxLayout):
+    def __init__(self, **kwargs):
+        super(LogPage, self).__init__(**kwargs)
+        self.user_database = {}  # Dictionary to store user credentials
+
     def login(self):
         username = self.ids.username_input.text
         password = self.ids.password_input.text
@@ -27,10 +31,17 @@ class LogPage(BoxLayout):
             return
         
         # Check login credentials
-        if username in self.user_database and self.user_database[username] == password:
+        if self.check_credentials(username, password):
             self.show_popup('Success', 'Login successful!')
+            self.switch_to_class_homepage(username)
         else:
             self.show_popup('Error', 'Invalid username or password.')
+    
+    def check_credentials(self, username, password):
+        # Check credentials
+        if username in self.user_database and self.user_database[username] == password:
+            return True
+        return False
     
     def signup(self):
         username = self.ids.username_input.text
@@ -54,6 +65,15 @@ class LogPage(BoxLayout):
                       size_hint=(None, None), size=(400, 200))
         popup.open()
 
+    def switch_to_class_homepage(self, username):
+        if username.startswith("t!"):
+            App.get_running_app().switch_to_teacher_classpage()
+        else:
+            App.get_running_app().switch_to_student_classpage()
+
+class ClassPage(BoxLayout):
+    pass
+
 class FitnessApp(App):
     def build(self):
         self.homepage = Homepage()
@@ -71,6 +91,20 @@ class FitnessApp(App):
     def switch_to_login(self):
         self.homepage.clear_widgets()
         self.homepage.add_widget(self.loginpage)
+
+    def switch_to_teacher_classpage(self):
+        self.homepage.clear_widgets()
+        self.homepage.add_widget(TeacherClassPage())
+
+    def switch_to_student_classpage(self):
+        self.homepage.clear_widgets()
+        self.homepage.add_widget(StudentClassPage())
+
+class TeacherClassPage(BoxLayout):
+    pass
+
+class StudentClassPage(BoxLayout):
+    pass
 
 if __name__ == '__main__':
     Window.size=(397,697)
